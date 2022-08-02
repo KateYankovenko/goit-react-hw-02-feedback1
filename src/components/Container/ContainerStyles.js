cbd347142cd1787a1ac5237fe444623ca2912ac2
Kate Yankovenko <k.yankovenko@ukr.net>
2022-08-02 21:25:17 +0200

    message

--- src/components/Container/ContainerStyles.js
+++ src/components/Container/ContainerStyles.js
@@ -2,11 +2,10 @@ import styled from 'styled-components';
 
 const ContainerStyle = styled.div`
   max-width: 1200px;
-  margin-left: auto;
-  margin-right: auto;
   padding-left: 15px;
   padding-right: 15px;
   background: #fff888;
+  box-shadow: 4px 17px 55px 5px rgba(179,190,287,2)
 `;
 
 export default ContainerStyle;
