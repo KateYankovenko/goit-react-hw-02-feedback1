6f006410a41da1a4290bdb91a1b089ce644c580f
Kate Yankovenko <k.yankovenko@ukr.net>
2022-08-02 20:48:14 +0200

    container

---
+++ src/components/Container/ContainerStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ContainerStyle = styled.div`
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  background: #fff888;
+`;
+
+export default ContainerStyle;
